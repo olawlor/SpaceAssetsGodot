d4fd0ea44d8cb13c4d63e42baf6abed5b8e596f7
Orion Lawlor <lawlor@alaska.edu>
2022-09-08 19:07:31 -0800

    Fix head tilting using a vector dot product.

--- Examples/Rotation/KeyboardMouseLook.gd
+++ Examples/Rotation/KeyboardMouseLook.gd
@@ -6,9 +6,10 @@ var last_pos = Vector2(-1,-1) # pixels where mouse last seen
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var rot=Vector3(0,0,0) # XYZ rotation amount (radians)
-	var move=Vector3(0,0,0) # local XYZ motion
-	var speed=5.0*delta   # meters/frame motion speed
+	var rot=Vector3(0,0,0) # XYZ rotation amount (radians this frame)
+	var move=Vector3(0,0,0) # local XYZ motion (meters this frame)
+	var mouse_speed=0.01; # look speed in radians / pixel
+	var speed=5.0*delta   # motion speed in meters/frame
 	
 	# Rotate based on mouse look
 	var cur_pos = get_viewport().get_mouse_position()
@@ -16,7 +17,6 @@ func _process(delta):
 	if (last_pos.x==-1):
 		dpos = Vector2(0,0)
 	last_pos = cur_pos
-	var mouse_speed=0.01; # radians / pixel
 	if Input.is_mouse_button_pressed(2): # right click
 		rot.y += -mouse_speed * dpos.x;
 		rot.x += -mouse_speed * dpos.y;
@@ -38,17 +38,18 @@ func _process(delta):
 	transform.origin += move.y*transform.basis.y;
 	transform.origin += move.z*transform.basis.z;
 
+	# Fix head tilting, by rotating the x axis to lie flat
+	var flatX = transform.basis.x;
+	flatX.y=0; # flat against horizon
+	rot.z += transform.basis.y.dot(flatX);
+
 	# Apply the rotations to our basis vectors:
 	transform.basis.y += rot.x*transform.basis.z;
 	transform.basis.z -= rot.x*transform.basis.y;
 	transform.basis.z += rot.y*transform.basis.x;
 	transform.basis.x -= rot.y*transform.basis.z;
-	
-	#var fix_tilt = transform.basis.y.x; # radians of rotation 
-	#rot.z += fix_tilt
 	transform.basis.x += rot.z*transform.basis.y;
 	transform.basis.y -= rot.z*transform.basis.x;
-	
 	
 	# Fix up any scale change or skew
 	transform.basis=transform.basis.orthonormalized()
